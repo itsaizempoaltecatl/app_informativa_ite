14e1db674b33764ae93096f667f3c57f8dddfd1e
Itsai <itsai@ensenada.gob.mx>
2022-04-01 00:15:13 -0700

    bottom navbar edit

--- lib/historia/historia_menu.dart
+++ lib/historia/historia_menu.dart
@@ -1,14 +1,26 @@
+import 'package:app_informativa_ite/classes/indexes.dart';
 import 'package:flutter/material.dart';
 
-class History extends StatelessWidget {
+class History extends StatefulWidget {
   const History({Key? key}) : super(key: key);
 
   @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      padding: const EdgeInsets.fromLTRB(20, 30, 20, 10),
-      color: Colors.cyanAccent,
-    );
+        width: double.infinity,
+        height: double.infinity,
+        padding: const EdgeInsets.fromLTRB(20, 30, 20, 10),
+        color: Colors.cyanAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(onPressed: () {}, child: Text("Antecendentes")),
+          ],
+        ));
   }
 }
--- lib/principal/home_page.dart
+++ lib/principal/home_page.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 
 import 'package:line_icons/line_icons.dart';
 
+import '../historia/antecedentes.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({
     Key? key,
@@ -16,61 +18,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentScreen = 0;
-
-  Map screens = {
+  int currentPage = 0;
+  final Map screens = {
     0: {'name': 'Historia', 'screen': const History()},
     1: {'name': 'Directorio', 'screen': const Directory()},
     2: {'name': 'Aviso de privacidad', 'screen': const Privacy()},
-    3: {'name': 'Manual de identidad grafica', 'screen': const Manual()}
+    3: {'name': 'Manual de identidad grafica', 'screen': const Manual()},
+    4: {'name': 'Antecedentes', 'screen': const Past()}
   };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('${screens[currentScreen]['name']}')),
-          backgroundColor: const Color(0xFF1b396a),
-        ),
-        body: screens[currentScreen]['screen'],
-        bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(
-            canvasColor: const Color(0xFF1b396a),
-          ),
-          child: BottomNavigationBar(
-            items: const [
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    LineIcons.history,
-                    color: Colors.white,
-                  ),
-                  label: 'Historia'),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    LineIcons.addressBook,
-                    color: Colors.white,
-                  ),
-                  label: 'Directorio'),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    LineIcons.fileInvoice,
-                    color: Colors.white,
-                  ),
-                  label: 'Aviso'),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    LineIcons.book,
-                    color: Colors.white,
-                  ),
-                  label: 'Manual')
-            ],
-            currentIndex: currentScreen,
-            onTap: (index) {
-              setState(() {
-                currentScreen = index;
-              });
-            },
-          ),
-        ));
+      appBar: AppBar(
+        title: Center(child: Text('${screens[currentPage]['name']}')),
+        backgroundColor: const Color(0xFF1b396a),
+      ),
+      body: screens[currentPage]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF1b396a),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        selectedFontSize: 20,
+        unselectedFontSize: 15,
+        iconSize: 25,
+        currentIndex: currentPage,
+        onTap: (index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                LineIcons.history,
+                color: Colors.white,
+              ),
+              label: 'Historia'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                LineIcons.addressBook,
+                color: Colors.white,
+              ),
+              label: 'Directorio'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                LineIcons.fileInvoice,
+                color: Colors.white,
+              ),
+              label: 'Aviso'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                LineIcons.book,
+                color: Colors.white,
+              ),
+              label: 'Manual')
+        ],
+      ),
+    );
   }
 }
