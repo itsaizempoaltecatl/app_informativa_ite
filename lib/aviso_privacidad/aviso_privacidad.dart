72c9582a77c8cce24966391ec8a95b755078e983
KevinMataITE <al18760440@ite.edu.mx>
2022-03-31 17:10:32 -0700

    aviso de privacidad
    se creo aviiso de privacidad

---
+++ lib/aviso_privacidad/aviso_privacidad.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Privacy extends StatelessWidget {
+  const Privacy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.fromLTRB(20, 30, 20, 10),
+      color: Colors.cyanAccent,
+      child: Column(
+        children: [
+          RichText(
+            text: const TextSpan(
+              text: 'Aviso de Privacidad ',
+              
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
