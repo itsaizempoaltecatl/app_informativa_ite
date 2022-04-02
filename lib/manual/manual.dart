27570df3df2626bf0edddc6c8ac61310e66b8896
Itsai <itsai@ensenada.gob.mx>
2022-04-01 18:09:43 -0700

    registro avance pantallas

--- lib/manual/manual.dart
+++ lib/manual/manual.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
 class Manual extends StatelessWidget {
   const Manual({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(
-        backgroundColor: Color(0xFF1b396a),
-        middle: Text(
-          'Manual de identidad gráfica',
-          style: TextStyle(color: Colors.white, fontSize: 20),
+        navigationBar: const CupertinoNavigationBar(
+          backgroundColor: Color(0xFF1b396a),
+          middle: Text(
+            'Manual de identidad gráfica',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
         ),
-      ),
-      child:SfPdfViewer.network('https://www.ensenada.tecnm.mx/wp-content/uploads/2020/06/Manual_de_Identidad_Grafica-1.pdf')
-    );
+        child: SfPdfViewer.network(
+            'https://www.ensenada.tecnm.mx/wp-content/uploads/2020/06/Manual_de_Identidad_Grafica-1.pdf'));
   }
 }
